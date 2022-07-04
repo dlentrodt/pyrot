006d708a654bc17f58759f9f0b0cd0e39bf8fb19
Dominik Lentrodt <dominik@lentrodt.com>
2022-07-04 10:38:35 +0200

    Added pyproject.toml and setup.cfg.

--- setup.py
+++ setup.py
@@ -1,28 +1,11 @@
-"""
-Copyright (C) 2022 Dominik Lentrodt
-
-This file is part of pyrot.
-
-pyrot is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-pyrot is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with pyrot.  If not, see <http://www.gnu.org/licenses/>.
-"""
-
-
 from setuptools import setup
 
 package_data = dict(pyrot=['*.py',
                            'pyrot/*.py',
                            ])
+
+if __name__ == "__main__":
+      setup()
 
 setup(name='pyrot',
       version='0.0.0',
